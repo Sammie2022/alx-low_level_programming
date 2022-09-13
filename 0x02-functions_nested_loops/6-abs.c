93dbe61ebee57a0768c7b0e3c545d7013517a6ca
Sammie2022 <samuelnganganganga26@gmail.com>
2022-09-13 13:45:01 -0700

    task

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,7 +5,7 @@
  *@n: is the int that we will use for the argument of the function
 Return: 1 if greater than 0, 0 if zero, -1 if less than 0
  */
-int _abs(int);
+int _abs(int)
 {
 	if (n < 0)
 		return (n * -1);
