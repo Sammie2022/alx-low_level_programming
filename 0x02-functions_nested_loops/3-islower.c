c20bea09d525441089bd13263e08f7401f38bf04
Sammie2022 <samuelnganganganga26@gmail.com>
2022-09-13 13:30:08 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,8 @@
 /**
  * _islower -  returns 1 if lowercase, 0 otherwise.
  * Return: 0 for uppercase 1 for lowercase
- * **/
-int _islower(int c);
+ */
+int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
