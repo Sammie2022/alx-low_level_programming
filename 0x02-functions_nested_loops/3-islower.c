fc42478d523f77b9b5812ec020b225fc233c8c31
Sammie2022 <samuelnganganganga26@gmail.com>
2022-09-13 13:33:47 -0700

    task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _islower -  returns 1 if lowercase, 0 otherwise.
+ * @c: contains value to be compared
  * Return: 0 for uppercase 1 for lowercase
  */
 int _islower(int c)
