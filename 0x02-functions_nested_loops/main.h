0d127d9ef727461001c7b16a08efda4f2a1d132a
Sammie2022 <samuelnganganganga26@gmail.com>
2022-09-14 02:31:46 -0700

    task

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
-#ifndef HOLBERTON_IS_INCLUDED
-#define HOLBERTON_IS_INCLUDED
+#fndef MAIN_H
+#define MAIN_H
 
-int _putchar(char);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -15,4 +15,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif	
+#endif
