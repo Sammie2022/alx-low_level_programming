ab6e4f60fada4af8e3426c9fe30ef6bd88ed5e97
Sammie2022 <samuelnganganganga26@gmail.com>
2022-09-14 07:59:58 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *  * print_triangle - prints triangle
- *   * @size: print size
- *    * Return: Always 0.
- *     */
+ * print_triangle - prints triangle
+ * @size: print size
+ * Return: Always 0.
+ */
 
 void print_triangle(int size)
 
