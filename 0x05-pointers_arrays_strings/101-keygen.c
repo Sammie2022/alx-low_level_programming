ae22f9cd7720bdaef542c601970c5896e63fd2b1
Sammie2022 <samuelnganganganga26@gmail.com>
2022-09-20 02:17:44 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,19 +10,10 @@
  */
 int main(void)
 {
-	char c,
+	int num;
 
-	int x
-
-srand(time(0));
-while (x <= 2645)
-{
-	c = rand() % 128;
-		x += c;
-putchar(c);
-}
-
-putchar(2772 - x)
-
-	Return(0);
+	srand(time(0));
+	num = rand();
+	printf("%i\n", num);
+	return (0);
 }
