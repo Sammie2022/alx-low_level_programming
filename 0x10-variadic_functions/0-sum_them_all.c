11eaca66c0e567c71ea4ca8a2ef2f47f0575cb46
Sammie2022 <samuelnganganganga26@gmail.com>
2022-10-14 07:57:01 -0700

    Tasks

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,46 +1,30 @@
-#include "3-calc.h"
+#include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
- * main - program that perfroms simple operations
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
- */
-int main(int argc, char *argv[])
+*sum_them_all - sums all its parameters
+*@n: first parameter
+*
+*Return: 0 if n is null
+* or sum of parameters in other case
+*/
+
+int sum_them_all(const unsigned int n, ...)
 {
-	int arg1, arg2, result;
-	char o;
-	int (*func)(int, int);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+va_list li;
+int sum = 0;
+unsigned int i;
 
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3]);
+va_start(li, n);
 
-	func = get_op_func(argv[2]);
+if (n != 0)
+{
+for (i = 0; i < n; i++)
+sum += va_arg(li, int);
+}
 
-	if (!func)
-	{
-		printf("Error\n");
-		exit(99);
-	}
+va_end(li);
+return (sum);
 
-	o = *argv[2];
-
-	if ((o == '/' || o == '%') && arg2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	result = func(arg1, arg2);
-
-	printf("%d\n", result);
-
-	return (0);
 }
